4e597602884405231ee871d92e2afb0b5b3119d7
ItsRaza <razaabbas768@gmail.com>
2020-08-06 01:32:20 +0500

    coordinates

--- try.py
+++ try.py
@@ -1,1 +1,25 @@
-print("Hello")
+from geopy.geocoders import Nominatim
+from geopy.distance import great_circle
+
+
+geolocator = Nominatim(user_agent="Saffona")
+location1 = geolocator.geocode("87/B Alamgir Society Malir Karachi")
+print('\n')
+pointA = location1.address
+print("point A=", pointA)
+A = (location1.latitude, location1.longitude)
+print(A)
+print('\n')
+
+location2 = geolocator.geocode("175 5th Avenue NYC")
+
+print('\n')
+
+pointB = location2.address
+print("point B=", pointB)
+B = (location2.latitude, location2.longitude)
+print(B)
+
+# calculating distance
+
+print("Distance =", great_circle(A, B).kilometers, "km")
